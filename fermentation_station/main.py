15f54a278068d7db70e4164704dc7451e789e33d
lukasb27 <lukasball@live.co.uk>
2025-02-02 21:26:07 +0000

    feat: adding fastapi support for getting temperature

--- fermentation_station/main.py
+++ fermentation_station/main.py
@@ -1,8 +1,17 @@
-from fermentation-station.models.controller import Controller 
+import uvicorn
+from fastapi import FastAPI
+
+from fermentation_station.models.controller import Controller
+from fermentation_station.routers import temp, root
+
+app = FastAPI()
+
+app.include_router(temp.router)
+app.include_router(root.router)
+
 
 def main():
-    controller = Controller()
-    print(controller.perform_action())
+    uvicorn.run(app, host="127.0.0.1", port=8000)
 
 if __name__ == "__main__":
     main()
---
+++ fermentation_station/routers/temp.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter, Depends
+
+from fermentation_station.models.temp_sensor import TempSensor
+
+router = APIRouter()
+
+def get_temp_sensor() -> TempSensor:
+    return TempSensor()
+
+@router.get("/temp")
+async def get_temp(temp_sensor: TempSensor = Depends(get_temp_sensor)):
+    return {"temp": temp_sensor.get_value()}
