a4a5eaf09dab131d559ca343966196ee7825cc91
Lukas Ball <lukasball@live.co.uk>
2026-02-16 20:57:53 +0000

    feat: create ephemeral environment on PR (#17)
    Adding the ability for this repo to create and push argo cd application yamls to the argo control repo.

---
+++ utils/argo_app_writer.py
@@ -0,0 +1,30 @@
+from jinja2 import Environment, FileSystemLoader
+import os
+
+def create_template(branch: str) -> str:
+    branch = branch.lower()
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    template_dir = os.path.join(current_dir, "templates")
+    env = Environment(loader=FileSystemLoader(template_dir))
+    template = env.get_template("argocd-application.template.yaml")
+
+    output = template.render(
+        APP_NAME=f"fermentation-station-agent-{branch}",
+        NAMESPACE=branch,
+        IMAGE=f"lukasball/fermentation-station-agent:{branch}"
+    )
+
+    return output
+
+def write_output_to_file(path_to_file: str, data: str) -> None:
+    with open(path_to_file, "w") as f:
+        f.write(data)
+
+def main():
+    branch = os.environ["BRANCH"]
+    filename = f"{branch}.yaml"
+    output = create_template(branch)
+    write_output_to_file(filename, output)
+
+if __name__ == '__main__':
+    main()
