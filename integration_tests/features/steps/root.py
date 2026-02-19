fa7ca1569a028fcc41134c55b6d5ff5af35415eb
lukasb27 <lukasball@live.co.uk>
2026-02-19 22:03:39 +0000

    fix: integ tests should query the service dns not localhost

--- integration_tests/features/steps/root.py
+++ integration_tests/features/steps/root.py
@@ -4,7 +4,7 @@ import requests
 @when('we curl the {location} of the web app')
 def step_when_switch_blender_on(context, location):
     location_map = {'root': ''}
-    context.response = requests.get(f'http://localhost:8000/{location_map[location]}')
+    context.response = requests.get(f'http://fermentation-station-agent-service:8000/{location_map[location]}')
 
 @then('it should return {status_code} status code')
 def step_then_should_transform_into(context, status_code):
