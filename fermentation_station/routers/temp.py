085f94200ca9d09e4247ccdf6a1ccc84e363a634
Lukas Ball <lukasball@live.co.uk>
2026-03-19 21:34:40 +0000

    feat: add new factory for getting temp sensor objects (#23)

--- fermentation_station/routers/temp.py
+++ fermentation_station/routers/temp.py
@@ -1,14 +1,11 @@
 from fastapi import APIRouter, Depends
 
-from fermentation_station.services.temp_sensor import TempSensor
+from fermentation_station.abcs.sensor import Sensor
+from fermentation_station.dependencies import get_temp_sensor
 
 router = APIRouter()
 
 
-def get_temp_sensor() -> TempSensor:
-    return TempSensor()
-
-
 @router.get("/temp")
-async def get_temp(temp_sensor: TempSensor = Depends(get_temp_sensor)):
+async def get_temp(temp_sensor: Sensor = Depends(get_temp_sensor)):
     return {"temp": temp_sensor.get_value()}
